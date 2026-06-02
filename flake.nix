abce06e36a1661f980cb55ff9b63b9a7c7fe4edd
Nicholas Malcolm <bubylou@pm.me>
2026-06-01 20:16:10 -0400

    gathers-api nix package

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
@@ -30,6 +30,45 @@
           ];
 
           RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/src";
+        };
+
+        packages = rec {
+          default = gathers-api;
+          gathers-api = pkgs.callPackage (
+            {
+              sqlite,
+              rustPlatform,
+              cacert,
+            }:
+              rustPlatform.buildRustPackage {
+                name = "gathers-api";
+                src = ./.;
+                cargoLock = {lockFile = ./Cargo.lock;};
+
+                buildInputs = [
+                  cacert
+                  sqlite
+                ];
+
+                checkFlags = [
+                  # Scryfall API not reachable in nix sandbox
+                  "--skip=systems::scryfall::tests"
+                ];
+              }
+          ) {};
+        };
+
+        apps = rec {
+          default = gathers-cli;
+          gathers-api = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.gathers-api;
+            exePath = "/bin/server";
+          };
+
+          gathers-cli = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.gathers-api;
+            exePath = "/bin/gathers";
+          };
         };
       });
 }
